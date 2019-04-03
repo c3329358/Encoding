5860008d4bd4e0eb3039bffbc9be5a5845e5160c
Jackson McCombie <c3329358@uon.edu.au>
2019-04-03 08:44:53 +0000

    best attempt so far

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    char str[500] = {"z"};
-    int key = 1;
-    int i;
-    for(int i=1; str[i] = 0; i++){
+    int key, i;
+    char str[500];
+    printf("Enter Thingo\n");
+    scanf("%s", &str);
+    key = 1;
+    for(i = 1; str[i] == 0; i++){
         str[i] = str[i] + key;
-        if(str[i]>=123){
+        if(str[i] == 123){          //little z to a
             str[i] = 97;
-        }        
-    }
+        } else if(str[i] == 91){      //Capital Z to A
+            str[i] == 65;
+        } else if(str[i]==32){              //dealing with white spaces
+            str[i] = str[i];
+        }          
+        }
     printf("%s", str);
-  return 0;
-  }
+    return 0;
+}
+
