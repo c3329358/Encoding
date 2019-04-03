957560204f28ca1f71ee01d51641ec9827ee1184
Jackson McCombie <c3329358@uon.edu.au>
2019-04-03 00:31:38 +0000

    pseudo code

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 #include <string.h>
 
 int main(){
+    //int arrays
+    char orgsent[500]; //original sentence before encrypting it
+    
+    //int character values (might be done in arrays)
+    //scan message
+    //set all characters to there assigned number values
+    //plus 1 to the numbers and reassign the new values to their respected letter
+    //print new message
   return 0;
   }
