3dd449706d1b3048019bda51acd9fe65bc72f76b
Jackson McCombie <c3329358@uon.edu.au>
2019-04-04 03:49:10 +0000

    Decrypts and Encodes

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 int main(){
     char op;
     int i;
-    printf("Enter What You Wanna Do:\n 1) Substitution Cipher       2).....\n 3).....       4).....\n");
-    sscanf ("%c", &op);               //choosing function or case
-    char str[500]= {"abc abc"};     //message 
+    char str[500]= {"szbo tnfmmt ljoeb cbe mnbp"};     //message 
+    op = 2;                        //choosing function or case
     switch(op){
         case 1:     
-        //substitute c
+        //substitute cipher         WORKS
             for(i = 0; str[i] != '\0'; i++){
                 str[i] = str[i] + 1;
                 if(str[i] ==33){             //dealing with white spaces
@@ -20,13 +19,27 @@ int main(){
             }                               //end of for loop
             printf("%s", str);              
             return 0;
-        break;
+            break;
         case 2:
+        //substitute decrypt    Works
+        for(i = 0; str[i] != '\0'; i++){
+            str[i] = str[i] - 1;
+            if(str[i] ==31){             //dealing with white spaces
+                str[i] = 32;
+            } else if(str[i] == 96){    //little a to z
+                str[i] = 122;
+            }else if(str[i] == 64){      //Capital A to Z
+                str[i] = 90;
+            }  
+        }                               //end of for loop
+            printf("%s", str);              
+            return 0;
         break;
         case 3: //insert .....
         break;
         case 4: //insert .....
         break;
         default: printf("Your Dumb Enter An Actual Operation Stupid\n");
+        
     }
 }
